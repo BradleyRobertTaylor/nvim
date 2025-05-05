c91dad63cb4ad9d416ef7f4a6f2bdc44eeb0eb0b
Bradley Taylor <qzi@qlik.com>
2025-05-05 08:41:53 -0700

    tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,8 +9,15 @@ return {
           blink_cmp = true,
         },
       }
-
-      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('tokyonight').setup {}
+      vim.cmd.colorscheme 'tokyonight'
     end,
   },
 }
